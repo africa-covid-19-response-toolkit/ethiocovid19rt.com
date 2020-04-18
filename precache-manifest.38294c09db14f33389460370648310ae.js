fbcabebec60eaf0944fa05e4505f7bb4681db62b
Naod Yeheyes <naodya@gmail.com>
2020-04-18 14:44:52 -0400

    Updates

---
+++ precache-manifest.38294c09db14f33389460370648310ae.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c61fec83b22f5353b85d1b2de1c0da",
+    "url": "/Ethiopia-COVID19/ecrt-website/index.html"
+  },
+  {
+    "revision": "e58d62e3f086336d9705",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/css/main.892826ed.chunk.css"
+  },
+  {
+    "revision": "f80a5e0a1a0224bd6501",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/js/2.fca523ce.chunk.js"
+  },
+  {
+    "revision": "f3559a585326a5bcf7c913f6e55005fd",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/js/2.fca523ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58d62e3f086336d9705",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/js/main.4473460b.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/js/main.4473460b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a9e8a22926b73022e8",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/js/runtime-main.351584fa.js"
+  },
+  {
+    "revision": "aee89369088d66b7d0cc46ca15fb9fd4",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/ENELEGNA.aee89369.JPG"
+  },
+  {
+    "revision": "1aa6308cc5cc01b18a4ffdd717f6c6d8",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/ecrt_logo.1aa6308c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/Ethiopia-COVID19/ecrt-website/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
