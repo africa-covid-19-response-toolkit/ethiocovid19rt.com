a36435e7a70f0790e0e0eed077fbd977b66569f5
Naod Yeheyes <naodya@gmail.com>
2020-04-17 23:21:40 -0400

    Updates

---
+++ precache-manifest.5bba6c00b7d3c85b0219bd032369f3dd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2329188160977833218803ad5b8dca53",
+    "url": "/argon-design-system-react/index.html"
+  },
+  {
+    "revision": "75cd2515beb4808d1a36",
+    "url": "/argon-design-system-react/static/css/main.a9fd7914.chunk.css"
+  },
+  {
+    "revision": "3ba98bad447c5ee7ca3c",
+    "url": "/argon-design-system-react/static/js/2.a329c42f.chunk.js"
+  },
+  {
+    "revision": "123c36955d3b020d1348e93b9ab88993",
+    "url": "/argon-design-system-react/static/js/2.a329c42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cd2515beb4808d1a36",
+    "url": "/argon-design-system-react/static/js/main.92b94490.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/argon-design-system-react/static/js/main.92b94490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e64ddb72e5c997dbe6d",
+    "url": "/argon-design-system-react/static/js/runtime-main.ed131ea3.js"
+  },
+  {
+    "revision": "1aa6308cc5cc01b18a4ffdd717f6c6d8",
+    "url": "/argon-design-system-react/static/media/ecrt_logo.1aa6308c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/argon-design-system-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/argon-design-system-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/argon-design-system-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/argon-design-system-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/argon-design-system-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/argon-design-system-react/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/argon-design-system-react/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "0ec264785c9b6efbed2651d84105f436",
+    "url": "/argon-design-system-react/static/media/landing.0ec26478.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/argon-design-system-react/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/argon-design-system-react/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/argon-design-system-react/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/argon-design-system-react/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/argon-design-system-react/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "278a0d250cc46de3a4f65a0791479e71",
+    "url": "/argon-design-system-react/static/media/profile.278a0d25.jpg"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/argon-design-system-react/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/argon-design-system-react/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/argon-design-system-react/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/argon-design-system-react/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/argon-design-system-react/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
