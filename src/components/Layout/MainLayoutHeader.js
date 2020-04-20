4fee78ad50a60c1af9ee83bcc32fbe5806e46a20
Naod Yeheyes <naodya@gmail.com>
2020-04-20 10:43:53 -0400

    Added title and description to main layout.

---
+++ src/components/Layout/MainLayoutHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// reactstrap components
+import { Container, Row, Col } from 'reactstrap';
+
+const MainLayoutHeader = ({ title, description }) => {
+  return (
+    <Container className="py-lg-md d-flex">
+      <div className="col px-0">
+        <Row>
+          <Col lg="12">
+            <h1 className="display-3 text-black">{title}</h1>
+            <p className="text-black">{description}</p>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
+};
+
+export default MainLayoutHeader;
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -4,7 +4,14 @@ import Projects from 'components/Projects';
 
 const ProjectsPage = (props) => {
   return (
-    <MainLayout>
+    <MainLayout
+      title="Our Projects"
+      description="ECRT is currently working across 8 key verticals that aim to support
+    Ethiopia’s COVID-19 containment efforts. The projects have broad
+    ranges going from raising awareness and delivering care packages to
+    families struggling in this pandemic to building surveillance and
+    reporting dashboards to aid the fight against COVID-19."
+    >
       <Projects {...props} />
     </MainLayout>
   );
