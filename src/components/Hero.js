50a326ddea286b061039301b48139859334adf4d
yehualashset abebe <yehualashetab@gmail.com>
2020-04-25 04:10:59 +0530

    fix isse on image display  and add margion top for carousel

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,14 +12,13 @@ import {
   UncontrolledCarousel,
 } from 'reactstrap';
 import { truncate } from 'lodash';
-
 const items = [
   {
     id: 1,
     heading: 'Africa COVID-19 Response Toolkit Launched',
     summary: `We’re excited to announce the first release of the Africa COVID-19 Response Toolkit (ACRT), a set of open source projects developed by an international group of volunteer software engineers, designers, and PMs that allow any government or public entity to get up and running with a software toolset to respond to COVID-19.
     `,
-    image: 'assets/img/theme/africa_covid19_2.svg',
+    image: `${require('assets/img/theme/africa_covid19_2.svg')}`,
     buttonLink:
       'https://github.com/africa-covid-19-response-toolkit/community/tree/readme-refresh',
   },
@@ -27,7 +26,8 @@ const items = [
     id: 2,
     heading: 'ECRT Partners with EDTF',
     summary: `ADTF Emergency COVID-19 or EDTF COVID-19 is established by the EDTF Advisory Council in recognition of the new challenge and existential threat that coronavirus represents to the wellbeing and livelihood of the Ethiopian people and in particular the most disadvantaged segments of the population.`,
-    image: `'https://www.ethiopiatrustfund.org/wp-content/uploads/2018/09/edtf-yellow-alt.png`,
+    // image: `'https://www.ethiopiatrustfund.org/wp-content/uploads/2018/09/edtf-yellow-alt.png`,
+    image: `${require('assets/img/theme/africa_covid19_2.svg')}`,
     actionbutton:'Donate',
     buttonLink:
       'https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page',
@@ -38,7 +38,7 @@ const items = [
     summary: `Argon Design System comes with four pre-built pages to help you
     get started faster. You can change the text and images and
     you're good to go.'`,
-    image: 'assets/img/theme/africa_covid19_2.svg',
+    image: `${require('assets/img/theme/africa_covid19_2.svg')}`,
     buttonLink:
       'https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page',
   },
@@ -66,6 +66,7 @@ const Hero = (props) => {
   };
 
   const slides = items.map((item) => {
+    
     return (
       <CarouselItem
         onExiting={() => setAnimating(true)}
@@ -77,7 +78,7 @@ const Hero = (props) => {
             <Col className="mb-lg-auto" lg="6">
               <h2 className="text-dark font-weight-bold">{item.heading}</h2>
               <p className="lead text-dark mt-4">
-              {truncate(item.summary, { length: 400 })}
+              {truncate(item.summary, { length: 200 })}
               </p>
               <Button
                 className="btn-white mt-4"
@@ -91,8 +92,8 @@ const Hero = (props) => {
               <img
                 alt="..."
                 className="img-center img-fluid"
-            //    src={require(item.image)}
-                src={require('assets/img/theme/africa_covid19_2.svg')}
+                src={item.image}
+                // src={require('assets/img/theme/africa_covid19_2.svg')}
               />
             </Col>
           </Row>
