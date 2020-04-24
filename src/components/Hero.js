c275a38ecf0ad79c79dace733e7b18cf5510d22d
Nati A <natiabebe12@gmail.com>
2020-04-24 09:30:00 -0700

    Added carousel

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,125 @@
-import React from 'react';
-// reactstrap components
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+  Button,
+  Container,
+  Row,
+  Col,
+  UncontrolledCarousel,
+} from 'reactstrap';
 
-const Hero = () => {
+const items = [
+  {
+    id: 1,
+    heading: 'Slide 1',
+    summary: `Argon Design System comes with four pre-built pages to help you
+    get started faster. You can change the text and images and
+    you're good to go.'`,
+    image: 'assets/img/theme/africa_covid19_2.svg',
+    buttonLink: 'https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page'
+  } ,{
+    id: 2,
+    heading: 'Slide 2',
+    summary: `Argon Design System comes with four pre-built pages to help you
+    get started faster. You can change the text and images and
+    you're good to go.'`,
+    image: `'assets/img/theme/africa_covid19_2.svg'`,
+    buttonLink: 'https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page'
+  }, {
+    id: 3,
+    heading: 'Slide 3',
+    summary: `Argon Design System comes with four pre-built pages to help you
+    get started faster. You can change the text and images and
+    you're good to go.'`,
+    image: 'assets/img/theme/africa_covid19_2.svg',
+    buttonLink: 'https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page'
+  },
+];
+
+const Hero = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+
+  
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.id}
+      >
+        <Container className="py-md">
+          <Row className="justify-content-between align-items-center">
+            <Col className="mb-lg-auto" lg="6">
+            <h1 className="text-white font-weight-light">
+               {item.heading}
+              </h1>
+              <p className="lead text-white mt-4">
+               {item.summary}
+              </p>
+              <Button
+                className="btn-white mt-4"
+                color="default"
+                href={item.buttonLink}
+              >
+                Read More
+              </Button>
+            </Col>
+            <Col className="mb-5 mb-lg-0" lg="5">
+            <img
+                    alt="..."
+                    className="img-center img-fluid"
+                    src={require('assets/img/theme/africa_covid19_2.svg')}
+                  />
+            </Col>
+          </Row>
+        </Container>
+      </CarouselItem>
+    );
+  });
+
   return (
-    <Container className="py-lg-md d-flex">
-      <div className="col px-0">
-        <Row>
-          <Col lg="12">
-            <h1 className="display-3 text-black">
-              Ethiopia
-              <span className="text-black">COVID-19 Response Team</span>
-            </h1>
-            <p className="text-black">
-              We are a global volunteer team of engineers, health professionals,
-              designers, marketing professionals, architects, product managers,
-              and other industry professionals. We have mobilized to build and
-              support over 15+ open source projects such as; creating a database
-              for case tracking, developing public awareness campaigns, building
-              bots, facilitating enhanced delivery logistics, retrofitting
-              underutilized buildings into treatment centers, and responding to
-              many other needs to combat COVID-19 in Ethiopia.
-            </p>
-          </Col>
-        </Row>
-      </div>
-    </Container>
+    <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
   );
 };
 
