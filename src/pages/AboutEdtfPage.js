b725964b1cf5de7c14522e334d58ac138fe434d1
asdotme <asm.ece@gmail.com>
2020-04-27 10:26:21 -0500

    Adding page for EPDF Announcement

---
+++ src/pages/AboutEdtfPage.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import MainLayout from 'components/Layout/MainLayout';
+
+const AboutEdtfPage = () => {
+  return (
+    <MainLayout  title="About EDTF">
+      <div style={{padding: '12px 0'}}>
+        <h5>
+          ETHIOPIA DIASPORA TRUST FUND (EDTF) and Ethiopia COVID-19 Response
+          Team (ECRT)
+        </h5>
+        <h6>Partnership Note</h6>
+        <h6>Purpose</h6>
+        <p>
+          The Joint Ethiopia Diaspora Trust Fund (EDTF) and Ethiopia COVID-19
+          Response Team (ECRT) fund, EDTF x ECRT, is established by the EDTF
+          Advisory Council in partnership with the volunteer Ethiopian COVID-19
+          Response Team, in recognition of the new challenge and existential
+          threat that coronavirus (i.e. SARS-COV-2) represents to the wellbeing
+          and livelihood of the Ethiopian people and in particular the most
+          disadvantaged segments of the population.
+        </p>
+        <p>
+          This initiative will support the ECRT mission to help meaningfully
+          flatten the curve, help make more readily available and support
+          adoption technological solutions, and creatively address communal
+          challenges that make preventive measures especially difficult in
+          Ethiopia.
+        </p>
+        <p>
+          EDTF x ECRT conforms with the aligned objective of EDTF COVID -19 to
+          support first responders and healthcare professionals and meeting the
+          critical needs of disadvantaged
+        </p>
+
+        <h6>Operational Modality</h6>
+        <p>
+          The campaign against COVID-19, particularly in countries like Ethiopia
+          that face institutional and financial resource challenges, requires
+          effective national coordination of the activities to be undertaken. As
+          a result, the EDTF x ECRT fund will be primarily guided by the work
+          done by our nonprofit and healthcare partners on the ground.
+        </p>
+        <p>
+          In recognition of other organizations that have already initiated
+          activities to support the national COVID-19 campaign through medical
+          advisory and financial support. Whenever the opportunity exists, EDTF
+          COVID-19 will work in partnership with such organizations while
+          ensuring the transparent and accountable reporting of the funds
+          contributed by its Ethiopian Diaspora and other donors.
+        </p>
+        <p>
+          EDTF COVID-19 will be established as a separate window in the EDTF
+          donation portal so as to enable ECRT’s donors to contribute
+          specifically to the initiatives under ECRT Strategic pillars. The
+          separate window will also facilitate separate and identifiable
+          transparent reporting of donations received in the ECRT account and
+          the amount used out of it.
+        </p>
+
+        <h6>Approval Process</h6>
+        <p>
+          The EDTF x ECRT fund is not a project-based intervention similar to
+          the regular EDTF donations. To appropriately allocate the available
+          funds, approval will be sent to the full ECRT Operations Team
+          requesting a one-day approval or objection with quorum vote
+          (three-fourths present). The approved amount will be used to directly
+          pay suppliers or other organizations for the procurement of similar
+          items through a competitive solicitation.
+        </p>
+        <p>
+          The approved purchases will be sent to the EDTF Secretariat for
+          dispersal; in quantities of 10K or more.
+        </p>
+        <h6>Payments</h6>
+        <p>
+          Similar to the EDTF COVID-19 fund, the EDTF x ECRT fund will be
+          governed by the EDTF Secretariat and Advisory Council. ECRT agrees to
+          contribute 10K towards EDTF operations costs associated with the
+          account in appreciation for their governance of the account.
+        </p>
+        <p>
+          ECFT will identify partners on the ground will supply EDTF with the
+          information necessary to disperse funds according to EDTF
+          requirements. ECRT will also follow EDTF standards for invoice format
+          and EDTF will seed the ECRT account with $50,000 USD and ECRT is
+          responsible for identifying donors and raising the necessary funds to
+          execute on our initiatives.
+        </p>
+        <p>
+          As funds are collected, ECRT will allocate them towards the priorities
+          listed in the Strategic Pillars:
+        </p>
+        <ol type="1">
+          <li>
+            Enabling compliance with public health guidelines
+            <ol type="a">
+              <li>Awareness</li>
+              <li>Handwashing and hand sanitizers</li>
+              <li>Face masks for every person</li>
+              <li>Care package delivery for vulnerable </li>
+            </ol>
+          </li>
+          <li>
+            Supporting health/frontline workers with resources to ensure they
+            have access to:
+            <ol type="a">
+              <li>Personal protective equipment (PPE)</li>
+              <li>Medical equipment</li>
+              <li>Emotional support services</li>
+              <li>Adequate housing and transportation</li>
+            </ol>
+          </li>
+          <li>
+            Facilitating knowledge exchange between regions and countries
+            <ol type="a">
+              <li>
+                Developing COVID-19 awareness content to inform citizens in
+                local languages
+              </li>
+              <li>Encouraging the sharing of ideas through Civic Engagement</li>
+              <li>Free and open-source resources</li>
+            </ol>
+          </li>
+          <li>
+            Forming mission-aligned partnerships for amplified impact
+            <ol type="a">
+              <li>Nonprofits and volunteer organizations</li>
+              <li>Product and service providers</li>
+              <li>Professional associations</li>
+              <li>
+                Governmental agencies that align with public health and
+                community response
+              </li>
+              <li>
+                Using our platform to help existing efforts gain visibility and
+                resources (mutual aid)
+              </li>
+            </ol>
+          </li>
+          <li>
+            {' '}
+            Contribute meaningfully to ongoing response efforts to digitize
+            Africa/ digitizing response efforts
+            <ol type="a">
+              <li>
+                Develop digital tools that optimize public health response
+                efforts
+              </li>
+              <li>
+                Identify gaps in response efforts that can be met with
+                technology
+              </li>
+              <li>
+                Inspire data-driven, evidence-based decision making and policy
+                change
+              </li>
+              <li>
+                Drive adoption of technical tools by health sectors across the
+                continent
+              </li>
+            </ol>
+          </li>
+        </ol>
+
+        <h6>Duration of the EDTF Emergency COVID-19</h6>
+        <p>
+          The EDTF x ECRT Partnership subaccount will be open for the duration
+          of the time COVID-19 continues to be an existential threat to the
+          Ethiopian people.
+        </p>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default AboutEdtfPage;
