a8cafefeccaa824121a9200cee78d6624b9aff86
asdotme <asm.ece@gmail.com>
2020-05-01 07:41:14 -0500

    scan all logos in partners impl

--- src/components/Partners/LogoNames.js
+++
@@ -1,5 +0,0 @@
-const LogoNames = [
-    //Add only name of logos which exist in /assets/img/partners/
-]
-
-export default  LogoNames;
--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,24 +1,35 @@
-import LogoNames from './LogoNames';
 import React from 'react';
+let listOfImages = [];
 
-const Partners = () => {
-  const items = LogoNames.map((item) => (
-    <div>
-      <img alt="..." src={require('assets/img/partners/' + item)} />
-    </div>
-  ));
-
-  if (LogoNames.length > 0) {
-    return (
-      <div className="partners-section  ">
-        <div className="title">
-          <h2 class="display-3">Our Partners</h2>
-        </div>
-        <div className="partners-list">{items}</div>
-      </div>
+class Partners extends React.Component {
+  importAll(r) {
+    return r.keys().map(r);
+  }
+  componentWillMount() {
+    listOfImages = this.importAll(
+      require.context('assets/img/partners', false, /\.(png|jpe?g|svg)$/)
     );
   }
-  return <></>
-};
+  render() {
+    const logos = listOfImages.map((image) => (
+      <div>
+        <img alt="..." src={image} />
+      </div>
+    ));
+
+    const content = (
+      <div>
+        <div className="partners-section  ">
+          <div className="title">
+            <h2 class="display-3">Our Partners</h2>
+          </div>
+          <div className="partners-list">{logos}</div>
+        </div>
+      </div>
+    );
+
+    return listOfImages.length ? content : '';
+  }
+}
 
 export default Partners;
